030947225a2c8359f7ce8f79ebffa04c5569901b
kivenkat <32029019+kivenkat@users.noreply.github.com>
2019-07-01 01:00:28 -0700

    Update customizer.sh

--- customizer.sh
+++ customizer.sh
@@ -1,8 +1,8 @@
 wget https://pypi.python.org/packages/source/s/setuptools/setuptools-7.0.tar.gz --no-check-certificate
 tar xzf setuptools-7.0.tar.gz
 cd setuptools-7.0
+sudo python setup.py install
 wget https://github.com/Azure/WALinuxAgent/archive/v2.2.36.zip
 unzip v2.2.36.zip
 cd WALinuxAgent-2.2.36
-#sudo python setup.py install
 sudo systemctl restart waagent
